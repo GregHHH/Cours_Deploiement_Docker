eeaa3e1bc5ddcfbe2a590c9735a48af3d0a80926
GregHHH <gregoryhaton93@outlook.fr>
2023-01-31 16:50:43 +0100

    added: muti input support

--- compare.sh
+++ compare.sh
@@ -1,9 +1,12 @@
-if [ $1 -gt 5 ]
+for var in "$@"
+do	
+if [ $var -gt 5 ]
 then
-echo "$1 est plus grand que 5"
-elif [ $1 -lt 5 ]
+echo "$var est plus grand que 5"
+elif [ $var -lt 5 ]
 then
-echo "$1 est plus petit que 5"
+echo "$var est plus petit que 5"
 else
-echo "$1 est égal à 5"
+echo "$var est égal à 5"
 fi
+done
