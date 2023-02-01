845fc4ee3bc49ad1e7c5a90bcddf767b70f1f011
GregHHH <gregoryhaton93@outlook.fr>
2023-02-01 14:53:08 +0100

    added: ping every host on the current network

--- basic_scripts/ping.sh
+++ basic_scripts/ping.sh
@@ -1,13 +1,14 @@
 #ipconfig getifaddr en0 
-
 IP=10.93.163 #.206
+
 > online_host.txt
 > offline_host.txt # reset the file before a new run
+
 start=`date +%s`
 
 for i in {0..254}
 do
-	if ping -q -W 1 -c 1 $IP.$i >/dev/null;
+	if ping -q -W 1 -c 1 $IP.$i >/dev/null # silence the output
 	then
 		echo $IP.$i >> online_host.txt
 	else
